18d2dea1e3fea9824f8ec8804ce5c5673825a178
Doniyorbek <programmmerqety1@gmail.com>
2024-06-04 23:01:44 +0500

    cors allowed true

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# cors
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
@@ -46,7 +49,8 @@ INSTALLED_APPS = [
     ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",# new
+    "django.middleware.common.CommonMiddleware", # new
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
